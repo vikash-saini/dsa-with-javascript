cd8060f49fb0c3509f54b16c17049ed0819797c6
Saini <vikash.saini@accenture.com>
2024-01-31 09:23:45 +0530

    dsa with JS

---
+++ #3 - Recursion/3-palindrome.js
@@ -0,0 +1,14 @@
+// Ques 3 : Given an integer x, return true if x is a palindrome, and false otherwise.
+// Input: x = 121      ----->>>>>      Output: true;
+
+function isPalindrome(str) {
+  str = str.replace(/[^a-z0-9]/i, "").toLowerCase();
+  const len = str.length;
+
+  if (len <= 1) return true;
+  if (str[0] !== str[len - 1]) return false;
+  return isPalindrome(str.slice(1, -1));
+}
+
+console.log(isPalindrome("75257"));
+console.log("121".slice(1, -1));
